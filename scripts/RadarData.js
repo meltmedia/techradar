d38e67384e1efabc68d28f8443ade114a6679dbb
Devon Tackett <devon.tackett@gmail.com>
2013-08-28 07:31:02 -0700

    Updated with first pass of radar data

--- scripts/RadarData.js
+++ scripts/RadarData.js
@@ -1,5 +1,14 @@
 /* globals define */
 define(function() {
+
+    function createItem(name, r, t, movement) {
+        return {
+            "name":name, 
+            "pc":{"r":r,"t":t},
+            "movement":movement
+        };
+    }
+
     return {
         //This is the title for your window tab, and your Radar
         title : "Technology Radar",
@@ -55,98 +64,106 @@ define(function() {
                 "left" : 45,
                 "top" : 18,
                 "color" : "#8FA227",
+                // [Techniques] (90 - 180)
                 "items" : [ 
-                    {"name":"Database based Integration", "pc":{"r":350,"t":135},"movement":"t"},
-                    {"name":"Scrum certification", "pc":{"r":350,"t":95},"movement":"c", "url":"http://www.google.com"},
-                    {"name":"Incremental data warehousing", "pc":{"r":250,"t":165},"movement":"c"},    
-                    {"name":"DevOps", "pc":{"r":250,"t":110},"movement":"c"},    
-                    {"name":"Polygot Programming", "pc":{"r":180,"t":170},"movement":"c"},    
-                    {"name":"Automation of technical tests", "pc":{"r":180,"t":155},"movement":"c"},    
-                    {"name":"Capability modelling", "pc":{"r":180,"t":125},"movement":"c"},   
-                    {"name":"Service choreography", "pc":{"r":180,"t":105},"movement":"c"},   
-                    {"name":"Continuous deployment", "pc":{"r":180,"t":100},"movement":"c"},   
-                    {"name":"Evolutionary architecture", "pc":{"r":120,"t":95},"movement":"c"},   
-                    {"name":"Coding architects", "pc":{"r":90,"t":170},"movement":"c"},   
-                    {"name":"Visualisation and metrics", "pc":{"r":80,"t":150},"movement":"c"},   
-                    {"name":"Web as platform", "pc":{"r":80,"t":110},"movement":"c"},   
-                    {"name":"Emergent design", "pc":{"r":80,"t":100},"movement":"c"},   
-                    {"name":"Evolutionary database", "pc":{"r":70,"t":170},"movement":"c"},   
-                    {"name":"Platform roadmaps", "pc":{"r":30,"t":100},"movement":"c"},   
-                    {"name":"Build pipelines", "pc":{"r":30,"t":160},"movement":"c"} 
+                    // [Hold]
+                    // [Assess]
+                    createItem('Performance testing', 230, 100, 't'),
+                    createItem('Load testing', 280, 120, 't'),
+                    createItem('Integration Testing', 210, 140, 't'),
+                    createItem('Self executable app containers', 215, 160, 't'),
+                    // [Trial]
+                    createItem('System Testing', 110, 100, 't'),
+                    createItem('Continuous Delivery', 110, 120, 't'),
+                    createItem('Smoke Tests', 140, 140, 't'),
+                    createItem('Mobile Web Apps', 160, 160, 't'),
+                    // [Adopt]
+                    createItem('Unit Testing', 50, 100, 't'),
+                    createItem('TDD', 90, 120, 't'),
+                    createItem('Responsive Design', 30, 140, 't'),
+                    createItem('Continuous Integration', 70, 140, 't'),
+                    createItem('Version control systems', 20, 160, 't')
                 ]
             },
             { "quadrant": "Tools",
                 "left": 1160-100+30,
                 "top" : 18,
                 "color" : "#587486",
+                // [Tools] (0 - 90)
                 "items" : [ 
-                    {"name":"ESB", "pc":{"r":390,"t":20},"movement":"t"},   
-                    {"name":"Intentional Programming", "pc":{"r":310,"t":10},"movement":"c"},   
-                    {"name":"Cross mobile platforms", "pc":{"r":280,"t":85},"movement":"c"},   
-                    {"name":"Github", "pc":{"r":280,"t":70},"movement":"c"},   
-                    {"name":"Restfulie", "pc":{"r":280,"t":50},"movement":"c"},   
-                    {"name":"RDF triple stores", "pc":{"r":280,"t":30},"movement":"c"},   
-                    {"name":"Apache camel", "pc":{"r":180,"t":85},"movement":"c"},   
-                    {"name":"Next gen test tools", "pc":{"r":180,"t":75},"movement":"c"},   
-                    {"name":"NoSQL", "pc":{"r":180,"t":65},"movement":"c"},   
-                    {"name":"Neo4j", "pc":{"r":180,"t":50},"movement":"c"},   
-                    {"name":"Message busses without smarts", "pc":{"r":160,"t":85},"movement":"c"},   
-                    {"name":"Puppet", "pc":{"r":130,"t":85},"movement":"c"},   
-                    {"name":"mongoDB", "pc":{"r":130,"t":55},"movement":"c"},   
-                    {"name":"Mercurial", "pc":{"r":130,"t":30},"movement":"c"},   
-                    {"name":"Git", "pc":{"r":130,"t":15},"movement":"c"},   
-                    {"name":"Squid", "pc":{"r":80,"t":85},"movement":"c"},   
-                    {"name":"ASP.NET MVC", "pc":{"r":80,"t":10},"movement":"c"},   
-                    {"name":"Subversion", "pc":{"r":30,"t":30},"movement":"c"}
+
+
+                    // [Hold]
+                    createItem('Ant', 340, 45, 't'),
+                    // [Assess]
+                    createItem('Gradle', 280, 70, 't'),
+                    // [Trial]
+                    createItem('Docker', 120, 20, 't'),
+                    // [Adopt]
+                    createItem('Require.js', 60, 10, 't'),
+                    createItem('Bower', 70, 20, 't'),
+                    createItem('Grunt', 60, 30, 't'),
+                    createItem('Maven', 55, 40, 't'),
+                    createItem('Chef', 90, 50, 't'),
+                    createItem('Vagrant', 60, 60, 't'),
+                    createItem('GitHub', 40, 70, 't')
                 ]
             },
             { "quadrant": "Platforms",
                 "left" :45,
                  "top" : (1200/2 + 18),
                 "color" : "#DC6F1D",
+                // [Platforms] (180 - 270)
                 "items" : [
-                    {"name":"Rich internet applications", "pc":{"r":390,"t":265},"movement":"c"},   
-                    {"name":"GWT", "pc":{"r":390,"t":250},"movement":"c"},   
-                    {"name":"IE8", "pc":{"r":390,"t":230},"movement":"c"},   
-                    {"name":"WS-* beyond basic profile", "pc":{"r":390,"t":190},"movement":"c"},   
-                    {"name":"Azure", "pc":{"r":290,"t":265},"movement":"c"},   
-                    {"name":"Mobile Web", "pc":{"r":275,"t":260},"movement":"t"},   
-                    {"name":"Google App Engine", "pc":{"r":290,"t":255},"movement":"c"},   
-                    {"name":"Application appliances", "pc":{"r":190,"t":245},"movement":"c"},   
-                    {"name":"Google as corporate platform", "pc":{"r":290,"t":200},"movement":"c"},   
-                    {"name":"GPGPU", "pc":{"r":190,"t":185},"movement":"t"},   
-                    {"name":"App containers", "pc":{"r":250,"t":260},"movement":"c"},   
-                    {"name":"OAuth", "pc":{"r":130,"t":260},"movement":"t"},   
-                    {"name":"RDFa", "pc":{"r":130,"t":250},"movement":"t"},   
-                    {"name":"Location based services", "pc":{"r":130,"t":230},"movement":"c"},   
-                    {"name":"iPad", "pc":{"r":130,"t":220},"movement":"c"},   
-                    {"name":"EC2 & S3", "pc":{"r":190,"t":250},"movement":"c"},   
-                    {"name":"Facebook as a business platform", "pc":{"r":190,"t":190},"movement":"c"},   
-                    {"name":"JVM as platform", "pc":{"r":90,"t":265},"movement":"c"},   
-                    {"name":"iPhone", "pc":{"r":90,"t":215},"movement":"c"},   
-                    {"name":"Android", "pc":{"r":90,"t":190},"movement":"c"},   
-                    {"name":"KVM", "pc":{"r":70,"t":260},"movement":"t"},   
-                    {"name":"Atom", "pc":{"r":70,"t":245},"movement":"t"},   
-                    {"name":"ALT.NET", "pc":{"r":90,"t":190},"movement":"c"},   
-                    {"name":"IE6 End of Life", "pc":{"r":30,"t":225},"movement":"c"}
+
+                    // [Hold]
+                    createItem('OSGi', 350, 190, 't'),
+                    createItem('Sling', 390, 225, 't'),
+                    createItem('JBoss', 320, 260, 't'),
+                    // [Assess]
+                    // [Trial]
+                    createItem('MongoDB', 220, 220, 't'),
+                    // [Adopt]
+                    createItem('Cadmium', 70, 190,'t'),
+                    createItem('Amazon AWS', 50, 220, 't'),
+                    createItem('MySQL', 80, 260, 't')
                 ]
             },
             { "quadrant": "Languages",
                 "color" : "#B70062",
                 "left"  : (1160-100+30),
                 "top" :   (1200/2 + 18),
+                // [Languages & Framework] (270 - 360)
                 "items" : [ 
-                    {"name":"Java language end of life", "pc":{"r":290,"t":355},"movement":"c"},   
-                    {"name":"F#", "pc":{"r":270,"t":330},"movement":"c"},   
-                    {"name":"Scala", "pc":{"r":290,"t":320},"movement":"c"},   
-                    {"name":"Clojure", "pc":{"r":260,"t":310},"movement":"c"},   
-                    {"name":"HTML 5", "pc":{"r":250,"t":275},"movement":"c"},   
-                    {"name":"DSLs", "pc":{"r":190,"t":340},"movement":"c"},   
-                    {"name":"Groovy", "pc":{"r":190,"t":280},"movement":"c"},   
-                    {"name":"C#4", "pc":{"r":90,"t":355},"movement":"c"},   
-                    {"name":"JRuby", "pc":{"r":90,"t":330},"movement":"c"},   
-                    {"name":"Javascript as a 1st class language", "pc":{"r":90,"t":275},"movement":"c"},   
-                    {"name":"Ruby", "pc":{"r":30,"t":282},"movement":"c"}
+
+
+                    // [Hold]
+                    createItem('J2EE', 350, 280, 't'),
+                    createItem('Flash', 340, 300, 't'),
+                    createItem('Flex', 380, 320, 't'),
+                    createItem('Dart', 310, 340, 't'),
+                    // [Assess]
+                    createItem('Dropwizard', 280, 285, 't'),
+                    createItem('Ruby', 250, 305, 't'),
+                    createItem('Go', 290, 315, 't'),
+                    createItem('Clojure', 290, 350, 't'),
+                    createItem('Grails', 250, 330, 't'),
+                    // [Trial]
+                    createItem('PHP', 120, 290, 't'),
+                    createItem('Drupal', 120, 310, 't'),
+                    createItem('Angular.js', 110, 330, 't'),
+                    createItem('Groovy', 110, 350, 't'),
+                    createItem('Python', 140, 320, 't'),
+                    // [Adopt]
+                    createItem('Require.js', 50, 280, 't'),
+                    createItem('Twitter Bootstrap', 60, 300, 't'),
+                    createItem('HTML 5', 40, 320, 't'),
+                    createItem('Docpad', 70, 320, 't'),
+                    createItem('Javascript', 40, 300, 't'),
+                    createItem('Node.js', 40, 340, 't'),
+                    createItem('Java', 20, 350, 't'),
+                    createItem('Objective C', 60, 350, 't')
+
                 ]
             }
         ]
